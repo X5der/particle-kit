a2aa5811da24938a92575f1853034597c2318101
Pluginer_Enc <z0502281868@gmail.com>
2017-09-09 08:33:34 -0800

    Update Main.php

--- src/Main.php
+++ src/Main.php
@@ -1,12 +1,15 @@
 <?php
 
-namespace LavaDripParticle;
+namespace Particle-kit;
 
 use pocketmine\plugin\PluginBase;
 use pocketmine\level\particle\LavaDripParticle;
+use pocketmine\level\particle\BubbleParticle;
+use pocketmine\level\particle\AngryVillagerParticle;
 use pocketmine\command\CommandSender;
 use pocketmine\command\Command;
 use pocketmine\utils\TextFormat as Color;
+
 class LavaDripParticle extends PluginBase{
   
   public function onEnable(){
@@ -17,6 +20,9 @@ class LavaDripParticle extends PluginBase{
     switch($cmd->getName()){
       case 'particle':
         $sender->getLevel()->addParticle(new LavaDripParticle($sender));
+        $sender->getLevel()->addParticle(new BubbleParticle($sender));
+        $sender->getLevel()->addParticle(new AngryVillagerParticle($sender));
+        
     }
   }
 }
